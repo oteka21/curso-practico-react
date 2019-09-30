07c23a8d4247d95f931ae98cfe8dfb2076826359
Leonardo <lfotecam@gmail.com>
2019-09-29 23:25:18 -0500

    Proyecto configurado para iniciar

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Helloworld } from './components/Helloworld'
+
+render(<Helloworld /> , document.querySelector('#app'))
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        overlay: {
+            warnings: true,
+            errors: true
+        },
+        // hot: true,
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "index.html"
+        })
+    ]
+}
